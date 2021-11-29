cbafab0b77d1ec64ac35bdf562b574e6fd4f7c3b
Kacper Czerwinski <czerwin92@gmail.com>
2021-11-29 13:08:20 +0100

    Cart Cleaned

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { ProductInShop } from 'src/app/models/product-model';
 import { CartService } from 'src/app/services/cart.service';
 
 @Component({
@@ -11,43 +10,10 @@ export class CardComponent implements OnInit {
   constructor(private cartService: CartService) {}
 
   readonly productsInCart$ = this.cartService.productListinCart$;
-  displayedColumns = [
-    'No.',
-    'Image',
-    'Name',
-    'Price',
-    'PriceTotal',
-    'Quantity',
-    'Actions',
-    'Delete',
-  ];
-
   innerWidth: any;
-  totalPrice$ = this.cartService.totalPrice$;
-
-  deleteProduct(product: ProductInShop) {
-    this.cartService.deleteProduct(product);
-  }
-  clearAll() {
-    if (confirm('Are You sure to clear cart?')) {
-      this.cartService.removeAllProducts();
-    }
-  }
-  addQuantity(product: ProductInShop) {
-    this.cartService.addQuantity(product);
-  }
-  reduceQuantity(product: ProductInShop) {
-    this.cartService.reduceQuantity(product);
-  }
-  hideContent(product: ProductInShop) {
-    this.cartService.hideContent(product);
-  }
-
   ngOnInit() {
     this.innerWidth = window.innerWidth;
-    this.cartService.clearHide();
   }
-
   @HostListener('window:resize') trackResolution() {
     this.innerWidth = window.innerWidth;
   }
---
+++ src/app/components/card/mobile/mobile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductInShop } from 'src/app/models/product-model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-mobile',
+  templateUrl: './mobile.component.html',
+  styleUrls: ['./mobile.component.scss'],
+})
+export class MobileComponent implements OnInit {
+  constructor(private readonly cartService: CartService) {}
+
+  productsInCart$ = this.cartService.productListinCart$;
+  totalPrice$ = this.cartService.totalPrice$;
+
+  clearAll() {
+    if (confirm('Are You sure to clear cart?')) {
+      this.cartService.removeAllProducts();
+    }
+  }
+  deleteProduct(product: ProductInShop) {
+    this.cartService.deleteProduct(product);
+  }
+  addQuantity(product: ProductInShop) {
+    this.cartService.addQuantity(product);
+  }
+  reduceQuantity(product: ProductInShop) {
+    this.cartService.reduceQuantity(product);
+  }
+
+  ngOnInit(): void {}
+}
