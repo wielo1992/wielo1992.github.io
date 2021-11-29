1f35d802d269bc791d43e0dbf1584d7bb7da90cb
Kacper Czerwinski <czerwin92@gmail.com>
2021-11-29 21:52:00 +0100

    Sum funkction rewrited

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,7 +12,6 @@ export class CartService {
     this.productListinCart$.subscribe((data) =>
       this.localStorage.addToLocalStorage(data)
     );
-    this.summPrice();
   }
   private readonly productListInCart = new BehaviorSubject<ProductInShop[]>([]);
   readonly productListinCart$ = this.productListInCart.asObservable();
